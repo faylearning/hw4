8dd4081cebd1feabd1bdf900ac76716f3fa61703
faylearning <fayliure@gmail.com>
2024-03-13 17:36:37 -0700

    equal paths submission

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -7,12 +7,55 @@
 using namespace std;
 
 
-// You may add any prototypes of helper functions here
+bool currPaths(Node* curr, int currTotal, int& total){
+    Node* currL = curr->left;
+    Node* currR = curr->right;
+    if ((currL != nullptr) || (currR != nullptr)){
+        currTotal++;
+        bool L, R;
+        if (currL != nullptr){
+            L = currPaths(curr->left, currTotal, total);
+            if (L == false){
+                return false;
+            }
+        }
+        if (currR != nullptr){
+            R = currPaths(curr->right, currTotal, total);
+            if (R == false){
+                return false;
+            }
+        }
+        return true;
+    }else{
+        if (total == 0){
+            total = currTotal;
+            return true;
+        }else{
+            if(total != currTotal){
+                return false;
+            }else{
+                return true;
+            }
+        }
+    }
+}
+
 
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+    Node* curr = root;
+    int lenCurr = 0;
+    int len = 0;
+
+    if (root == NULL){
+        return true;
+    }
+
+    bool res = currPaths(curr, lenCurr, len);
+    return res;
 
 }
+
+
 
